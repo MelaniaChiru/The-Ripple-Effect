90920b2fc4d2418b51be9ef57eceae933ee13a2c
ashre <ashrebelo21@gmail.com>
2026-01-10 14:14:01 -0500

    make default grid

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,14 +1,15 @@
 import { useState } from 'react'
 import '../styles/App.css'
 import Homepage from './Homepage'
+import Grid from './levelComponents/Grid.jsx'
 
 function App() {
-  const [currentPage, setCurrentPage] = useState("homepage");
+  const [currentPage, setCurrentPage] = useState("level");
 
   return (
     <>
       {currentPage == "homepage" && <Homepage setCurrentPage={setCurrentPage}/>}
-      <Grid />
+      {currentPage == "level" && <Grid />}
     </>
   )
 }
