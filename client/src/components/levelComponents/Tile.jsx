30e6afa80eda1a2cbd5fe5d6e02273140d13c13f
ashre <ashrebelo21@gmail.com>
2026-01-10 13:34:33 -0500

    add image size

--- client/src/components/levelComponents/Tile.jsx
+++ client/src/components/levelComponents/Tile.jsx
@@ -1,7 +1,7 @@
 function Tile ({id, type, imgPath}) {
     return (
         <div className="tile" id={id} data-type={type}>
-            <img src={imgPath} alt={type} width={"100px"} />
+            <img src={imgPath} alt={type} width={"50px"} />
         </div>
     );
 }
