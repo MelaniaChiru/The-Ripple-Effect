bf44bc8713751289d7590cc5d9ce3237cd9bb31e
ashre <ashrebelo21@gmail.com>
2026-01-10 13:01:05 -0500

    add tiles

--- client/src/components/levelComponents/Grid.jsx
+++ client/src/components/levelComponents/Grid.jsx
@@ -4,14 +4,18 @@ import './Grid.css';
 
 function Grid({level, size}) {
     return (
-        <>
-            <h1>Grid</h1>
-            <div className="grid">
-                {tiles.map((tile) => (
+        <section id='grid-section'>
+            <h1>Level {level}</h1>
+            <div id='tiles' style={{display: 'grid', gridTemplateColumns: `repeat(${size}, 100px)`, gridTemplateRows: `repeat(${size}, 100px)`, gap: '5px'}}>
+                {/* {tiles.map((tile) => (
                     <Tile key={tile.name} id={tile.id} type={tile.type} imgPath={tile.imgPath} />
-                ))}
+                ))} */}
+                <Tile id="tile-1" type="house" imgPath={House} />
+                <Tile id="tile-2" type="school" imgPath={House} />
+                <Tile id="tile-3" type="park" imgPath={House} />
+                <Tile id="tile-4" type="house" imgPath={House} />
             </div>
-        </>
+        </section>
     );
 }
 
