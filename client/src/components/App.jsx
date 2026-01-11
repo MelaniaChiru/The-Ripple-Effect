e20eeb70dcbb5e9480108dfcc3dc9257703d5435
ashre <ashrebelo21@gmail.com>
2026-01-11 13:03:46 -0500

    fix level 2

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -5,7 +5,7 @@ import LevelSelection from './LevelSelection'
 import LevelComponent from './LevelComponent'
 
 function App() {
-  const [currentPage, setCurrentPage] = useState("homepage");  
+  const [currentPage, setCurrentPage] = useState("level-2");  
   const [showIntro, setShowIntro] = useState(true);
 
   return (
@@ -15,6 +15,7 @@ function App() {
       {currentPage == "level-1" && <LevelComponent level="1" setCurrentPage={setCurrentPage}/>}
       {currentPage == "level-2" && <LevelComponent level="2" setCurrentPage={setCurrentPage}/>}
       {currentPage == "level-3" && <LevelComponent level="3" setCurrentPage={setCurrentPage}/>}
+      {currentPage == "level-4" && <LevelComponent level="4" setCurrentPage={setCurrentPage}/>}
 
       <div className="small-screen-blocker">
         <h1>To Create A Better World You Need A Bigger Canvas</h1>
