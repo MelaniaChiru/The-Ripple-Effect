d6ae5775305a425b8c7201aa67d4d1e34bcfb69f
ashre <ashrebelo21@gmail.com>
2026-01-11 13:26:25 -0500

    add level 4 selection

--- client/src/components/LevelSelection.jsx
+++ client/src/components/LevelSelection.jsx
@@ -3,7 +3,7 @@ import '../styles/LevelSelection.css';
 import LevelSelectionTile from './LevelSelectionTile';
 
 function LevelSelection({ setCurrentPage, showIntro, setShowIntro }) {
-    const levelsArr = [1, 2, 3];
+    const levelsArr = [1, 2, 3, 4];
 
     // --- COOKIE HELPERS (Moved to top for safety) ---
     const readProgressFromCookie = () => {
